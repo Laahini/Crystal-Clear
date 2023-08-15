f9b135a14bdf18e3e38ed55af6a2e8c8eff45935
Laahini <62526180+Laahini@users.noreply.github.com>
2023-08-15 14:47:29 -0400

    first page

--- Crystal Clear/ContentView.swift
+++ Crystal Clear/ContentView.swift
@@ -6,83 +6,59 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item a \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        NavigationStack {
+            ZStack {
+                Image("bkg")
+                    .resizable()
+                VStack {
+                    Text("Hi there!")
+                        .font(.system(size: 45))
+                    Text("Who are you?").font(.system(size: 35))
+                    
+                    Button("Volunteers") {
+                        
+                    }.font(.system(size: 36))
+                        .buttonStyle(.borderedProminent)
+                        .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568))
+                        .frame(width: 400.0, height: 60)
+                    Spacer()
+                        .frame(height: 30.0)
+                    Button("Organizations")
+                    {
+                        
+                    }.font(.system(size: 36))
+                        .buttonStyle(.borderedProminent)
+                        .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568))
+                        .frame(width: 400.0, height: 60)
+                    
+                    NavigationLink(destination: signIn())
+                    {
+                        Text("volunteers")
+                            .font(.largeTitle)
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    NavigationLink(destination: signIn())
+                    {
+                        Text("orgs")
+                            .font(.largeTitle)
                     }
+                
+                    
+
                 }
-            }
-            Text("Select items")
+            }.edgesIgnoringSafeArea([.top, .bottom])
         }
+        
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+    
+    
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
+struct Previews_ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
---
+++ Crystal Clear/signIn.swift
@@ -0,0 +1,20 @@
+//
+//  signIn.swift
+//  Crystal Clear
+//
+//  Created by Laahini Addagatla on 8/15/23.
+//
+
+import SwiftUI
+
+struct signIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct signIn_Previews: PreviewProvider {
+    static var previews: some View {
+        signIn()
+    }
+}
