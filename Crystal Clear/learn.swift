78c9c79f10c6f81f30694a2db6ad066c81b272ea
Laahini <62526180+Laahini@users.noreply.github.com>
2023-08-18 07:46:14 -0400

    changes

--- Crystal Clear/learn.swift
+++ Crystal Clear/learn.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct learn: View {
+    @State private var animalsClicked = false
+    @State private var childrenClicked = false
+    @State private var foodClicked = false
+    @State private var environmentClicked = false
+    var deviceWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Learning Hub")
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding(.top, 25.0)
+            HStack {
+                ZStack{
+                    Rectangle()
+                    Button("Animals") {
+                        animalsClicked.toggle()
+                    }
+                }
+                ZStack{
+                    Rectangle()
+                    Button("Children") {
+                        childrenClicked.toggle()
+                    }
+                }
+                ZStack{
+                    Rectangle()
+                    Button("Food") {
+                        foodClicked.toggle()
+                    }
+                }
+                ZStack{
+                    Rectangle()
+                    Button("Environment") {
+                        environmentClicked.toggle()
+                    }
+                }
+            }
+            .frame(height: 100.0)
+//            Button("summer"){
+//                                isSummerButtonClicked.toggle()
+//                            }
+//                            .font(.largeTitle)
+//                            .buttonStyle(.borderedProminent)
+//                            .tint(.black)
+//                            .frame(height: 75)
+//                            //add summer image if button is clicked
+//                            ZStack{
+//                                if(isSummerButtonClicked == true){
+//                                    Image("summer")
+//                                    .resizable()
+//                                    .scaledToFit()
+//                                    .edgesIgnoringSafeArea(.all)
+//                                }
+//                            }
+
+            
+            
+        }
     }
 }
 
--- Crystal Clear/volunteerHome.swift
+++ Crystal Clear/volunteerHome.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct volunteerHome: View {
+    var deviceWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Latest Projects")
+                .font(.title)
+                .padding(.top, 25.0)
+            ZStack {
+                Rectangle()
+                    .fill(Color(red: -0.002, green: 0.479, blue: 0.999)).padding(.all, 20.0).cornerRadius(20)
+                VStack{
+                    Text("Books Buddies")
+                        .font(.title)
+                    Text("By: Books for Fun")
+                    Text("Become a bookworm! We are looking for children books including picture books and chapter books from common series.")
+                        .frame(width: deviceWidth-50)
+                }
+            }
+            ZStack {
+                Rectangle()
+                    .fill(Color(red: -0.002, green: 0.479, blue: 0.999)).padding(.all, 20.0).cornerRadius(20)
+                VStack{
+                    Text("Books Buddies")
+                        .font(.title)
+                    Text("By: Books for Fun")
+                    Text("Become a bookworm! We are looking for children books including picture books and chapter books from common series.")
+                        .frame(width: deviceWidth-50)
+                }
+            }
+            ZStack {
+                Rectangle()
+                    .fill(Color(red: -0.002, green: 0.479, blue: 0.999)).padding(.all, 20.0).cornerRadius(20)
+                VStack{
+                    Text("Books Buddies")
+                        .font(.title)
+                    Text("By: Books for Fun")
+                    Text("Become a bookworm! We are looking for children books including picture books and chapter books from common series.")
+                        .frame(width: deviceWidth-50)
+                }
+            }
+        }
     }
 }
 
