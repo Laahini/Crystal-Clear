45e641eb242d74c0ef2a063e6c46ff8bfc13aa83
Laahini <62526180+Laahini@users.noreply.github.com>
2023-08-18 09:16:18 -0400

    changes 9:15

--- Crystal Clear/learn.swift
+++ Crystal Clear/learn.swift
@@ -12,6 +12,15 @@ struct learn: View {
     @State private var childrenClicked = false
     @State private var foodClicked = false
     @State private var environmentClicked = false
+    @Environment(\.openURL) var openURL
+    @State var factsAnimals = ["I am seventeen", "I have a younger brother", "I was born in India", "I have an art account on Instagram"]
+    @State var facta = ""
+    @State var factsChild = ["I am seventeen", "I have a younger brother", "I was born in India", "I have an art account on Instagram"]
+    @State var factc = ""
+    @State var factsFood = ["I am seventeen", "I have a younger brother", "I was born in India", "I have an art account on Instagram"]
+    @State var factf = ""
+    @State var factsEnviron = ["I am seventeen", "I have a younger brother", "I was born in India", "I have an art account on Instagram"]
+    @State var facte = ""
     var deviceWidth: CGFloat {
         UIScreen.main.bounds.width
     }
@@ -21,52 +30,256 @@ struct learn: View {
                 .font(.title)
                 .multilineTextAlignment(.center)
                 .padding(.top, 25.0)
-            HStack {
+            HStack (spacing: 5) {
                 ZStack{
-                    Rectangle()
-                    Button("Animals") {
+                    Rectangle().frame(height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                    Button("🐶") {
+                        if(childrenClicked==true){
+                            childrenClicked.toggle()
+                        }
+                        else if (foodClicked==true)
+                        {
+                            foodClicked.toggle()
+                        }
+                        else if (environmentClicked == true)
+                        {
+                            environmentClicked.toggle()
+                        }
                         animalsClicked.toggle()
                     }
+                    .font(.largeTitle)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.black)
                 }
-                ZStack{
-                    Rectangle()
-                    Button("Children") {
+                ZStack
+                {
+                    Rectangle().frame(height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                    Button("🧒") {
+                        if(animalsClicked==true){
+                            animalsClicked.toggle()
+                        }
+                        else if (foodClicked==true)
+                        {
+                            foodClicked.toggle()
+                        }
+                        else if (environmentClicked == true)
+                        {
+                            environmentClicked.toggle()
+                        }
                         childrenClicked.toggle()
-                    }
+                    }.font(.largeTitle)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.black)
                 }
                 ZStack{
-                    Rectangle()
-                    Button("Food") {
+                    Rectangle().frame(height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                    Button("🍇") {
+                        if(animalsClicked==true){
+                            animalsClicked.toggle()
+                        }
+                        else if (childrenClicked==true)
+                        {
+                            childrenClicked.toggle()
+                        }
+                        else if (environmentClicked == true)
+                        {
+                            environmentClicked.toggle()
+                        }
                         foodClicked.toggle()
-                    }
+                    }.font(.largeTitle)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.black)
+                        .frame(height: 75)
                 }
                 ZStack{
-                    Rectangle()
-                    Button("Environment") {
+                    Rectangle().frame(height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                    Button("🌱") {
+                        if(animalsClicked==true){
+                            animalsClicked.toggle()
+                        }
+                        else if (foodClicked==true)
+                        {
+                            foodClicked.toggle()
+                        }
+                        else if (childrenClicked == true)
+                        {
+                            childrenClicked.toggle()
+                        }
                         environmentClicked.toggle()
-                    }
+                    }.font(.largeTitle)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.black)
+                        .frame(height: 75)
                 }
-            }
-            .frame(height: 100.0)
-//            Button("summer"){
-//                                isSummerButtonClicked.toggle()
-//                            }
-//                            .font(.largeTitle)
-//                            .buttonStyle(.borderedProminent)
-//                            .tint(.black)
-//                            .frame(height: 75)
-//                            //add summer image if button is clicked
-//                            ZStack{
-//                                if(isSummerButtonClicked == true){
-//                                    Image("summer")
-//                                    .resizable()
-//                                    .scaledToFit()
-//                                    .edgesIgnoringSafeArea(.all)
-//                                }
-//                            }
+            }.padding(.all, 10.0).position(x: 196, y:60)
+            VStack {
+                    if(animalsClicked == true) {
+                        Text("Animal Resources")
+                            .multilineTextAlignment(.center)
+                            .font(.title)
+                        //Animal Shelters
+                        Button("Volunteer at Shelters"){
+                            openURL(URL(string: "https://www.instagram.com/laa.hini_a/")!)
+                        }
+                        .font(.title2)
+                        .buttonStyle(.borderedProminent)
+                        .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                        
+                        Button("Animal Abuse Hotline"){
+                            openURL(URL(string: "https://www.peta.org/about-peta/contact-peta/report-cruelty/")!)
+                        }
+                        .font(.title2)
+                        .buttonStyle(.borderedProminent)
+                        .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                        
+                        Button("50 Ways to Help"){
+                            openURL(URL(string: "https://www.humanesociety.org/resources/50-ways-help-animals")!)
+                        }
+                        .font(.title2)
+                        .buttonStyle(.borderedProminent)
+                        .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                    
+                        //facts
+                        Button("\t\tShocking Statistics\t\t"){
+                            let randomFactNumber = Int.random(in:0..<factsAnimals.count)
+                            facta = factsAnimals[randomFactNumber]
+                        }
+                        .font(.title2)
+                        .buttonStyle(.borderedProminent)
+                        .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50).frame(height: 50)
+                        //output
+                        Text(facta)
+                            .fontWeight(.semibold)
+                            .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568))
+                            .font(.headline)
+                    }
+                else if (childrenClicked == true)
+                {
+                    Text("Children Resources")
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                    //Animal Shelters
+                    Button("Volunteer at Shelters"){
+                        openURL(URL(string: "https://www.instagram.com/laa.hini_a/")!)
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                    
+                    Button("Animal Abuse Hotline"){
+                        openURL(URL(string: "https://www.peta.org/about-peta/contact-peta/report-cruelty/")!)
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                    
+                    Button("50 Ways to Help"){
+                        openURL(URL(string: "https://www.humanesociety.org/resources/50-ways-help-animals")!)
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                
+                    //facts
+                    Button("\t\tShocking Statistics\t\t"){
+                        let randomFactNumber = Int.random(in:0..<factsChild.count)
+                        factc = factsChild[randomFactNumber]
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50).frame(height: 50)
+                    //output
+                    Text(factc)
+                        .fontWeight(.semibold)
+                        .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568))
+                        .font(.headline)
 
-            
-            
+                }
+                else if (foodClicked == true)
+                {
+                    Text("Food Scarcity Resources")
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                    //Animal Shelters
+                    Button("Volunteer at Shelters"){
+                        openURL(URL(string: "https://www.instagram.com/laa.hini_a/")!)
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                    
+                    Button("Animal Abuse Hotline"){
+                        openURL(URL(string: "https://www.peta.org/about-peta/contact-peta/report-cruelty/")!)
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                    
+                    Button("50 Ways to Help"){
+                        openURL(URL(string: "https://www.humanesociety.org/resources/50-ways-help-animals")!)
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                
+                    //facts
+                    Button("\t\tShocking Statistics\t\t"){
+                        let randomFactNumber = Int.random(in:0..<factsFood.count)
+                        factf = factsFood[randomFactNumber]
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50).frame(height: 50)
+                    //output
+                    Text(factf)
+                        .fontWeight(.semibold)
+                        .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568))
+                        .font(.headline)
+
+                }
+                else if (environmentClicked == true)
+                {
+                    Text("Environment Resources")
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                    //Animal Shelters
+                    Button("Volunteer at Shelters"){
+                        openURL(URL(string: "https://www.instagram.com/laa.hini_a/")!)
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                    
+                    Button("Animal Abuse Hotline"){
+                        openURL(URL(string: "https://www.peta.org/about-peta/contact-peta/report-cruelty/")!)
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                    
+                    Button("50 Ways to Help"){
+                        openURL(URL(string: "https://www.humanesociety.org/resources/50-ways-help-animals")!)
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50)
+                
+                    //facts
+                    Button("\t\tShocking Statistics\t\t"){
+                        let randomFactNumber = Int.random(in:0..<factsEnviron.count)
+                        facte = factsEnviron[randomFactNumber]
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568)).frame(height: 50).frame(height: 50)
+                    //output
+                    Text(facte)
+                        .fontWeight(.semibold)
+                        .tint(Color(hue: 0.609, saturation: 0.834, brightness: 0.568))
+                        .font(.headline)
+
+                }
+            }.position(x: 196, y:0)
         }
     }
 }
